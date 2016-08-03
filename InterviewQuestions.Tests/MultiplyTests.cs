1ea42d03165769c94e127b84d4e8843323c96ac4
Eric Burcham <eburcham@eprod.com>
2016-08-03 15:56:28 -0500

    Finished the question.

--- InterviewQuestions.Tests/MultiplyTests.cs
+++ InterviewQuestions.Tests/MultiplyTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace InterviewQuestions.Tests
@@ -25,6 +27,50 @@ namespace InterviewQuestions.Tests
         {
             var result = Functions.Multiply(2, 3);
             result.Should().Be(6);
+        }
+
+        [Test]
+        public void MultiplyShouldReturnSixFromNegativeTwoTimesNegativeThree()
+        {
+            var result = Functions.Multiply(-2, -3);
+            result.Should().Be(6);
+        }
+
+        [Test]
+        public void MultiplyReturnsZeroWhenFirstOperandIsZero()
+        {
+            var result = Functions.Multiply(0, 1);
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void MultiplyShouldReturnZeroWhenSecondOperandIsZero()
+        {
+            var result = Functions.Multiply(1, 0);
+            result.Should().Be(0);
+        }
+
+
+        [Test]
+        public void MultiplyShouldRunInSimilarTimesForDramaticallyDifferentOperands()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var firstResult = Functions.Multiply(2, 100000000);
+            sw.Stop();
+            var firstTime = sw.ElapsedMilliseconds;
+
+            sw.Restart();
+
+            var secondResult = Functions.Multiply(100000000, 2);
+            sw.Stop();
+            var secondTime = sw.ElapsedMilliseconds;
+
+            var timeDifference = Math.Abs(firstTime - secondTime);
+
+            firstResult.Should().Be(secondResult);
+            timeDifference.Should().BeLessThan(5);
         }
     }
 }
--- InterviewQuestions/Functions.cs
+++ InterviewQuestions/Functions.cs
@@ -1,12 +1,56 @@
-﻿using System;
-
-namespace InterviewQuestions
+﻿namespace InterviewQuestions
 {
     public static class Functions
     {
         public static int Multiply(int a, int b)
         {
-            throw new NotImplementedException();
+            if (EitherOperandIsZero(a, b))
+            {
+                return 0;
+            }
+
+            var absA = Abs(a);
+            var absB = Abs(b);
+
+            if (LoopingIsFasterOnA(absA, absB))
+            {
+                var temp = absA;
+                absA = absB;
+                absB = temp;
+            }
+
+            var result = 0;
+            for (var i = 0; i < absB; ++i)
+            {
+                result += absA;
+            }
+
+            if (ResultShouldBeNegative(a, b))
+            {
+                return 0 - result;
+            }
+
+            return result;
+        }
+
+        private static bool LoopingIsFasterOnA(int absA, int absB)
+        {
+            return absA < absB;
+        }
+
+        private static bool EitherOperandIsZero(int a, int b)
+        {
+            return a == 0 || b == 0;
+        }
+
+        private static bool ResultShouldBeNegative(int a, int b)
+        {
+            return (a < 0 && b > 0) || (a > 0 && b < 0);
+        }
+
+        private static int Abs(int i)
+        {
+            return i >= 0 ? i : 0 - i;
         }
     }
 }
