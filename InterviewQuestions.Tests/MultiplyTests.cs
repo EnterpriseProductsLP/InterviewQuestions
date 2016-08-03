a4c9dd3cf3b0505c266b0e7e61fa4aa5e953819c
Eric Burcham <eburcham@eprod.com>
2016-08-03 13:11:41 -0500

    Initial project load.

---
+++ InterviewQuestions.Tests/MultiplyTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace InterviewQuestions.Tests
+{
+    [TestFixture]
+    public class MultiplyTests
+    {
+        [Test]
+        public void MultiplyShouldReturnNegativeSixFromNegativeTwoTimesThree()
+        {
+            var result = Functions.Multiply(-2, 3);
+            result.Should().Be(-6);
+        }
+
+        [Test]
+        public void MultiplyShouldReturnNegativeSixFromTwoTimesNegativeThree()
+        {
+            var result = Functions.Multiply(2, -3);
+            result.Should().Be(-6);
+        }
+
+        [Test]
+        public void MultiplyShouldReturnSixFromTwoTimesThree()
+        {
+            var result = Functions.Multiply(2, 3);
+            result.Should().Be(6);
+        }
+    }
+}
