b9e279ab885cee00bc873418de16210b4d3a0a7f
Eric Burcham <eburcham@eprod.com>
2016-08-09 16:23:33 -0500

    Removed ItemData

--- InterviewQuestions/Functions.cs
+++ InterviewQuestions/Functions.cs
@@ -6,7 +6,16 @@ namespace InterviewQuestions
     {
         public static int Multiply(int a, int b)
         {
-            throw new NotImplementedException();
+            var min= Math.Min(Math.Abs(a), Math.Abs(b));
+            var max = Math.Max(Math.Abs(a), Math.Abs(b));
+            int result = 0;
+            for (int i = 0; i < min; i++)
+                result += max;
+            if (a <= 0 && b <= 0)
+                result = result;
+            else if (a <= 0 || b <= 0)
+                result = -result;
+            return result;
         }
     }
 }
