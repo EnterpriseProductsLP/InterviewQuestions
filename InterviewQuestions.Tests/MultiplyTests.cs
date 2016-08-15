7a6992be6d808212c0d0b1b7d5105fae7f6cc331
Eric Burcham <eburcham@eprod.com>
2016-08-15 15:13:06 -0500

    Venkat finished the excercise.

--- InterviewQuestions.Tests/MultiplyTests.cs
+++ InterviewQuestions.Tests/MultiplyTests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System;
+using System.Diagnostics;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace InterviewQuestions.Tests
@@ -6,6 +8,27 @@ namespace InterviewQuestions.Tests
     [TestFixture]
     public class MultiplyTests
     {
+        [Test]
+        public void MultiplyReturnsZeroWhenFirstOperandIsZero()
+        {
+            var result = Functions.Multiply(0, 1);
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void MultiplyShouldReturnZeroWhenSecondOperandIsZero()
+        {
+            var result = Functions.Multiply(1, 0);
+            result.Should().Be(0);
+        }
+
+        [Test]
+        public void MultiplyShouldReturnSixFromTwoTimesThree()
+        {
+            var result = Functions.Multiply(2, 3);
+            result.Should().Be(6);
+        }
+
         [Test]
         public void MultiplyShouldReturnNegativeSixFromNegativeTwoTimesThree()
         {
@@ -21,10 +44,25 @@ namespace InterviewQuestions.Tests
         }
 
         [Test]
-        public void MultiplyShouldReturnSixFromTwoTimesThree()
+        public void MultiplyShouldRunInSimilarTimesForDramaticallyDifferentOperands()
         {
-            var result = Functions.Multiply(2, 3);
-            result.Should().Be(6);
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var firstResult = Functions.Multiply(2, 100000000);
+            sw.Stop();
+            var firstTime = sw.ElapsedMilliseconds;
+
+            sw.Restart();
+
+            var secondResult = Functions.Multiply(100000000, 2);
+            sw.Stop();
+            var secondTime = sw.ElapsedMilliseconds;
+
+            var timeDifference = Math.Abs(firstTime - secondTime);
+
+            firstResult.Should().Be(secondResult);
+            timeDifference.Should().BeLessThan(10);
         }
     }
 }
--- InterviewQuestions/Functions.cs
+++ InterviewQuestions/Functions.cs
@@ -6,7 +6,25 @@ namespace InterviewQuestions
     {
         public static int Multiply(int a, int b)
         {
-            throw new NotImplementedException();
+            int product = 0;
+            if (Math.Abs(a) > Math.Abs(b))
+            {
+                int c = a;
+                a = b;
+                b = c;
+            }
+            for (int i = 1; i <= Math.Abs(a); i++)
+            {
+                if (b < 0)
+                    product -= b;
+                else
+                    product += b;
+            }
+
+            if ((a < 0) || (b < 0))
+                return 0 - product;
+            else
+                return product;
         }
     }
 }
