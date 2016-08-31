ca65f03103cd3bea98b440f3844f7a96b8788b39
Eric Burcham <eburcham@eprod.com>
2016-08-31 11:52:18 -0500

    Finished the multiply function.

--- InterviewQuestions/Functions.cs
+++ InterviewQuestions/Functions.cs
@@ -6,7 +6,34 @@ namespace InterviewQuestions
     {
         public static int Multiply(int a, int b)
         {
-            throw new NotImplementedException();
+            var result = 0;
+
+            if (a == 0 || b == 0)
+            {
+                return 0;
+            }
+
+            var absA = Math.Abs(a);
+            var absB = Math.Abs(b);
+
+            if (absA > absB)
+            {
+                var temp = absA;
+                absA = absB;
+                absB = temp;
+            }
+
+            for (var i = 0; i < absA; i++)
+            {
+                result += absB;
+            }
+
+            if ((a < 0 && b > 0) || (a > 0 && b < 0))
+            {
+                return 0 - result;
+            }
+
+            return result;
         }
     }
 }
