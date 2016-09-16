eb804a881974163e83cf00a9ef3885cb16352577
Eric Burcham <eburcham@eprod.com>
2016-09-16 12:31:36 -0500

    Finished.

--- InterviewQuestions/Functions.cs
+++ InterviewQuestions/Functions.cs
@@ -6,7 +6,27 @@ namespace InterviewQuestions
     {
         public static int Multiply(int a, int b)
         {
-            throw new NotImplementedException();
+            var returnNegative = (a < 0 && b > 0) || (a > 0 && b < 0);
+
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if (b > a)
+            {
+                var tmp = a;
+                a = b;
+                b = tmp;
+            }
+
+            var result = 0;
+            for (int i = 0; i < b; i++)
+            {
+                result += a;
+            }
+
+            return returnNegative
+                ? 0 - result
+                : result;
         }
-    }
+   }
 }
