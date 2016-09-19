dbac131d511620bce8fe1ead9ee02dd1e7f7aea4
Eric Burcham <eburcham@eprod.com>
2016-09-19 15:06:14 -0500

    Candidate finished the question.

--- InterviewQuestions/Functions.cs
+++ InterviewQuestions/Functions.cs
@@ -1,12 +1,43 @@
-﻿using System;
-
-namespace InterviewQuestions
+﻿namespace InterviewQuestions
 {
     public static class Functions
     {
         public static int Multiply(int a, int b)
         {
-            throw new NotImplementedException();
+            var retVal = 0;
+
+            if (a > 0 && a < b)
+            {
+                for (var ind = 0; ind < a; ind++)
+                    retVal += b;
+            }
+            else if (b > 0 && b < a)
+            {
+                for (var ind = 0; ind < b; ind++)
+                    retVal += a;
+            }
+            else if (a > 0)
+            {
+                for (var ind = 0; ind < a; ind++)
+                    retVal += b;
+            }
+            else if (b > 0)
+            {
+                for (var ind = 0; ind < b; ind++)
+                    retVal += a;
+            }
+            else if (a < 0 && a > b)
+            {
+                for (var ind = a; ind < 0; ind++)
+                    retVal -= b;
+            }
+            else if(a < 0 && b < 0)
+            {
+               for (var ind = b; ind < 0; ind++)
+                    retVal -= a;
+            }
+
+            return retVal;
         }
     }
 }
