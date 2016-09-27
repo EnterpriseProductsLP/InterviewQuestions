3801bf95a7345b2a73849cbd853aebb398e82e12
Eric Burcham <eburcham@eprod.com>
2016-09-27 11:00:03 -0500

    Done

--- InterviewQuestions.Tests/MultiplyTests.cs
+++ InterviewQuestions.Tests/MultiplyTests.cs
@@ -9,38 +9,39 @@ namespace InterviewQuestions.Tests
     public class AllMultiplyTests
     {
         [Test]
-        public void MultiplyReturnsZeroWhenFirstOperandIsZero()
-        {
-            var result = Functions.Multiply(0, 1);
-            result.Should().Be(0);
-        }
-
-        [Test]
-        public void MultiplyShouldReturnZeroWhenSecondOperandIsZero()
-        {
-            var result = Functions.Multiply(1, 0);
-            result.Should().Be(0);
-        }
-
-        [Test]
         public void MultiplyShouldReturnFourFromTwoTimesTwo()
         {
-            var result = Functions.Multiply(2, 2);
-            result.Should().Be(4);
+            var result = Functions.Multiply(2, 3);
+            result.Should().Be(6);
         }
 
         [Test]
         public void MultiplyShouldReturnNegativeFourForNegativeTwoTimesTwo()
         {
-            var result = Functions.Multiply(-2, 2);
-            result.Should().Be(-4);
+            var result = Functions.Multiply(-2, 3);
+            result.Should().Be(-6);
         }
 
         [Test]
         public void MultiplyShouldReturnNegativeFourForTwoTimesNegativeTwo()
         {
-            var result = Functions.Multiply(2, -2);
-            result.Should().Be(-4);
+            var result = Functions.Multiply(2, -3);
+            result.Should().Be(-6);
+        }
+
+        [Test]
+        public void MultiplyShouldReturnNegativeFourForTwoTimesNegativeTwo1()
+        {
+            var result = Functions.Multiply(-2, -3);
+            result.Should().Be(6);
+        }
+
+        [Test]
+        public void Something()
+        {
+            var result = Functions.Multiply(Int32.MaxValue, 2);
+            var maxValue = Int32.MaxValue;
+            result.Should().Be(maxValue * 2);
         }
 
         [Test]
@@ -56,6 +57,28 @@ namespace InterviewQuestions.Tests
             sw.Restart();
 
             var secondResult = Functions.Multiply(100000000, 2);
+            sw.Stop();
+            var secondTime = sw.ElapsedMilliseconds;
+
+            var timeDifference = Math.Abs(firstTime - secondTime);
+
+            firstResult.Should().Be(secondResult);
+            timeDifference.Should().BeLessThan(5);
+        }
+
+        [Test]
+        public void MultiplyShouldRunInSimilarTimesForDramaticallyDifferentNegativeOperands()
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            var firstResult = Functions.Multiply(2, -100000000);
+            sw.Stop();
+            var firstTime = sw.ElapsedMilliseconds;
+
+            sw.Restart();
+
+            var secondResult = Functions.Multiply(-100000000, 2);
             sw.Stop();
             var secondTime = sw.ElapsedMilliseconds;
 
--- InterviewQuestions/Functions.cs
+++ InterviewQuestions/Functions.cs
@@ -1,43 +1,48 @@
-﻿namespace InterviewQuestions
+﻿using System;
+
+namespace InterviewQuestions
 {
     public static class Functions
     {
         public static int Multiply(int a, int b)
         {
-            var retVal = 0;
+            var result = 0;
+            //check which number is bigger.
+            int x;
+            int y;
 
-            if (a > 0 && a < b)
+            if (Math.Abs(a) > Math.Abs(b))
             {
-                for (var ind = 0; ind < a; ind++)
-                    retVal += b;
+                x = a;
+                y = b;
             }
-            else if (b > 0 && b < a)
+            else
             {
-                for (var ind = 0; ind < b; ind++)
-                    retVal += a;
-            }
-            else if (a > 0)
-            {
-                for (var ind = 0; ind < a; ind++)
-                    retVal += b;
-            }
-            else if (b > 0)
-            {
-                for (var ind = 0; ind < b; ind++)
-                    retVal += a;
-            }
-            else if (a < 0 && a > b)
-            {
-                for (var ind = a; ind < 0; ind++)
-                    retVal -= b;
-            }
-            else if(a < 0 && b < 0)
-            {
-               for (var ind = b; ind < 0; ind++)
-                    retVal -= a;
+                x = b;
+                y = a;
             }
 
-            return retVal;
+
+            //To address positive a and b
+
+            if (y > 0)
+            {
+                for (var i = 1; i <= y; i++)
+                {
+                    result = result + x;
+                }
+            }
+            else
+            {
+                for (var i = -1; i >= y; i--)
+                {
+                    result = result - x;
+                }
+            }
+
+            return result;
         }
+
+
     }
 }
