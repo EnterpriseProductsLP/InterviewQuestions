0159fba5bfb6cf6d68bb3351e7a6318811cebfbc
Eric Burcham <eburcham@eprod.com>
2016-10-24 10:50:49 -0500

    Finished basic solution.

--- InterviewQuestions/Functions.cs
+++ InterviewQuestions/Functions.cs
@@ -6,7 +6,28 @@ namespace InterviewQuestions
     {
         public static int Multiply(int a, int b)
         {
-            throw new NotImplementedException();
+            var product = 0;
+            bool productShouldBeNegative = (a > 0 && b < 0) || (a < 0 && b > 0);
+
+            var multiplier = Math.Abs(a);
+            var multiplicand = Math.Abs(b);
+
+            if (multiplier > multiplicand)
+            {
+                for (int i = 0; i < multiplicand; i++)
+                {
+                    product += multiplier;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < multiplier; i++)
+                {
+                    product += multiplicand;
+                }
+            }
+
+            return productShouldBeNegative ? 0 - product : product;
         }
     }
 }
