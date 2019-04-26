1aa3173b022be07827ac42b4b7f6e53fd2c63d3a
Eric Burcham <eburcham@eprod.com>
2019-04-26 10:57:29 -0500

    Removed tests.

--- InterviewQuestions.Tests/MultiplyTests.cs
+++ InterviewQuestions.Tests/MultiplyTests.cs
@@ -6,25 +6,5 @@ namespace InterviewQuestions.Tests
     [TestFixture]
     public class MultiplyTests
     {
-        [Test]
-        public void MultiplyShouldReturnSixFromTwoTimesThree()
-        {
-            var result = Functions.Multiply(2, 3);
-            result.Should().Be(6);
-        }
-
-        [Test]
-        public void MultiplyShouldReturnNegativeSixFromNegativeTwoTimesThree()
-        {
-            var result = Functions.Multiply(-2, 3);
-            result.Should().Be(-6);
-        }
-
-        [Test]
-        public void MultiplyShouldReturnNegativeSixFromTwoTimesNegativeThree()
-        {
-            var result = Functions.Multiply(2, -3);
-            result.Should().Be(-6);
-        }
     }
 }
