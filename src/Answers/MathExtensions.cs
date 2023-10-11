279917c6e024c2fac48b46df3cf067627ce60f96
Eric Burcham <eburcham@eprod.com>
2023-10-11 12:56:11 -0500

    Document the Times extension method.

--- src/Answers/MathExtensions.cs
+++ src/Answers/MathExtensions.cs
@@ -2,6 +2,15 @@
 
 public static class MathExtensions
 {
+    /// <summary>Multiplies two integers.</summary>
+    /// <param name="x">The first operand.</param>
+    /// <param name="y">The second operand.</param>
+    /// <returns>The product of x and y.</returns>
+    /// <remarks>
+    ///     When implementing this method:
+    ///     * You may not use the asterisk(*) symbol.
+    ///     * You may not use any linq statements.
+    /// </remarks>
     public static int Times(this int x, int y)
     {
         throw new NotImplementedException("This method to be implemented by the candidate.");
