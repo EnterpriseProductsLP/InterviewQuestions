dab23b666d42d5a2ceeec3229b410a08700761a5
Eric Burcham <eburcham@eprod.com>
2023-10-18 17:49:05 -0500

    add summation tests

---
+++ src/Tests/Summation/WhenSomeValuesArePositiveAndSomeAreNegative.cs
@@ -0,0 +1,14 @@
+﻿using Answers;
+
+namespace Tests.Summation;
+
+public class WhenSomeValuesArePositiveAndSomeAreNegative : IntFunctionTest
+{
+    protected override int ExpectedResult => 3;
+
+    protected override int GetResult()
+    {
+        var values = new[] {1, -2, 3, -4, 5};
+        return values.Sum();
+    }
+}
